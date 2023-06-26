6b6f3b96c3af374c161b95c41d3d75024c85cad2
Tarik Mey <tarik.mey@gmail.com>
2023-06-26 07:23:33 +0200

    adding eventlisteners to nav buttons and implementing the hide and unhide function to display the form

--- libraryApp.js
+++ libraryApp.js
@@ -26,3 +26,26 @@ function listLibraryBooks() {
 const firstBook = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 
 console.log(firstBook.info());
+
+// Navigation Buttons
+const newBookBtn = document.querySelector("#newBookBtn");
+const viewBooks = document.querySelector("#viewBooks");
+const findBook = document.querySelector("#findBook");
+
+
+// Webcontent 
+const form = document.querySelector("#form");
+
+
+//eventListeners for Navigation buttons
+newBookBtn.addEventListener("click", () => {
+    form.classList.remove("hideElement");
+});
+
+findBook.addEventListener("click", () => {
+    form.classList.add("hideElement");
+})
+
+viewBooks.addEventListener("click", () => {
+    form.classList.add("hideElement");
+})
