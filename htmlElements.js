fda1529d0e7206937a29ba25f008873d8a902ee5
Tarik Mey <tarik.mey@gmail.com>
2023-07-14 08:09:18 +0200

    adding home btn to navbar

--- htmlElements.js
+++ htmlElements.js
@@ -1,46 +1,49 @@
 const content = document.querySelector("#contentSection");
 const bottomNav = document.querySelector("#bottomNav");
 
-const libraryBtn = document.querySelector("#libraryBtn");
-const newBookBtn = document.querySelector("#newBookBtn");
+const homeBtn = document.querySelector(".homeBtn");
+const libraryBtn = document.querySelector(".libraryBtn");
+const newBookBtn = document.querySelector(".newBookBtn");
 
 // provided html element structure for the content section
-const newBookForm = '<form action="" method="post"><label for="author">Author</label><input id="author" type="text"></form>';
+const newBookForm = "<div id='form' class='grid-container'><form id='submitBook' action='' method='post'><label for='title'>what’s the books title?</label><input id='title' type='text' name='title' placeholder='What's the books title?' required><label for='author'>who is the author?</label><input id='author' type='text' name='author' placeholder='Who is the author?' required><label for='pages'>how many pages to read?</label><input id='pages' type='number' name='pages' min='1' placeholder='How many pages?' required><label for='read'>did you read it already?</label><input id='read' type='checkbox' name='finished'><label for='addBookCover'>what’s the books cover?</label><button id='addBookCover' name='addBookCover'>+</button><button id='submitBook-btn' type='submit'>Add book</button></form></div>";
+const libraryList = "<ul><li>book1</li><li>book2</li><li>book3</li></ul>";
+const homeScreen = "<h1>Home</h1>";
 
 
 // Event Listeners
+homeBtn.addEventListener("click", () => {
+    changeContent(homeScreen);
+})
+
 newBookBtn.addEventListener("click", () => {
-    const innerHTML = newBookForm;
-    // element must be a string when provided!
-    changeContent("form", innerHTML);
+    changeContent(newBookForm);
 })
 
 libraryBtn.addEventListener("click", () => {
-    changeContent();
+    changeContent(libraryList);
 })
 
-function appendElements(element, innerHTML) {
+// function appendElements(element, innerHTML) {
+function appendElements(contentToAdd) {
     // element must be a string when provided!
     // creating container as a dump element, which will be deleted every time the removeChild function is called
     content.innerHTML = '<div id="content-container"></div>';
     const contentContainer = document.querySelector("#content-container");
 
-    const elementToCreate = document.createElement(element);
-    elementToCreate.innerHTML = innerHTML;
-
-    contentContainer.appendChild(elementToCreate);
+    // new element to be added to the DOM
+    contentContainer.innerHTML = contentToAdd;
 }
 
 function removeElements() {
     content.removeChild(document.querySelector("#content-container"));
 }
 
-function changeContent(element, innerHTML) {
+function changeContent(contentToAdd) {
     // remove all elements from content section
     removeElements();
 
     // append new elements to content section
-    appendElements(element, innerHTML);
+    appendElements(contentToAdd);
 }
 
-// appendElements(content, "form", newBookForm);
