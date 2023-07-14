f0e7c015d6c15bbaa4b783cb8d8564844c5ea10f
Tarik Mey <tarik.mey@gmail.com>
2023-07-14 07:25:01 +0200

    on button click (library-btn, newBook-btn) the content in the DOM is changed dynamically

--- htmlElements.js
+++ htmlElements.js
@@ -2,27 +2,45 @@ const content = document.querySelector("#contentSection");
 const bottomNav = document.querySelector("#bottomNav");
 
 const libraryBtn = document.querySelector("#libraryBtn");
-const newBookBtnBtn = document.querySelector("#newBookBtn");
+const newBookBtn = document.querySelector("#newBookBtn");
 
 // provided html element structure for the content section
 const newBookForm = '<form action="" method="post"><label for="author">Author</label><input id="author" type="text"></form>';
 
 
-function appendElements(target, element, innerHTML) {
+// Event Listeners
+newBookBtn.addEventListener("click", () => {
+    const innerHTML = newBookForm;
     // element must be a string when provided!
+    changeContent("form", innerHTML);
+})
+
+libraryBtn.addEventListener("click", () => {
+    changeContent();
+})
+
+function appendElements(element, innerHTML) {
+    // element must be a string when provided!
+    // creating container as a dump element, which will be deleted every time the removeChild function is called
+    content.innerHTML = '<div id="content-container"></div>';
+    const contentContainer = document.querySelector("#content-container");
+
     const elementToCreate = document.createElement(element);
     elementToCreate.innerHTML = innerHTML;
-    // elementToCreate.innerHTML = '<form action="" method="post"><label for="author">Author</label><input id="author" type="text"></form>';
-    target.appendChild(elementToCreate);
+
+    contentContainer.appendChild(elementToCreate);
 }
 
-function removeElements(element) {
-    content.removeChild(element);
+function removeElements() {
+    content.removeChild(document.querySelector("#content-container"));
 }
 
-function changeContent() {
-    appendElements();
+function changeContent(element, innerHTML) {
+    // remove all elements from content section
     removeElements();
+
+    // append new elements to content section
+    appendElements(element, innerHTML);
 }
 
-appendElements(content, "form", newBookForm);
+// appendElements(content, "form", newBookForm);
