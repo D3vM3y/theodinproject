10e90470e6cb0056afaa6ac7389da37bfaa1095f
Tarik Mey <tarik.mey@gmail.com>
2023-07-17 19:01:22 +0200

    adding book btn functionality to book card

--- htmlElements.js
+++ htmlElements.js
@@ -33,29 +33,22 @@ const libraryBtn = document.querySelector(".libraryBtn");
 // const newBookBtn = document.querySelector(".newBookBtn");
 
 // provided html element structure for the content section
-// const newBookForm = "<div id='form' class='grid-container'><form id='submitBook' action='' method='post'><label for='title'>what’s the books title?</label><input id='title' type='text' name='title' placeholder='What's the books title?' required><label for='author'>who is the author?</label><input id='author' type='text' name='author' placeholder='Who is the author?' required><label for='pages'>how many pages to read?</label><input id='pages' type='number' name='pages' min='1' placeholder='How many pages?' required><label for='read'>did you read it already?</label><input id='read' type='checkbox' name='finished'><label for='addBookCover'>what’s the books cover?</label><button id='addBookCover' name='addBookCover'>+</button><button id='submitBook-btn' type='submit'>Add book</button></form></div>";
-// const libraryList = "<ul class='bookList'><li class='bookCard'>book1</li><li class='bookCard'>book2</li><li class='bookCard'>book3</li></ul>";
 const homeScreen = "<div class='welcome-note'><div class='banner'></div><span>hello,</span><span>username</span><div class='horizontalDivider'></div></div>";
 const scrollBox = document.querySelector(".scroll-box");
-// const contentContainer = document.querySelector("#contentContainer");
 
-
-
-// book card btns
 const bookBtns = document.querySelectorAll(".read-btn");
+
 
 
 // Event Listeners
 homeBtn.addEventListener("click", () => {
     const contentContainer = document.querySelector("#contentContainer");
-    // loadPage(contentContainer, homeScreen);
     createContentContainer(homeScreen);
     navbar.classList.add("homeNavbar");
 })
 
 libraryBtn.addEventListener("click", () => {
     const contentContainer = document.querySelector("#contentContainer");
-    // loadPage(contentContainer, createLibrary());
     createContentContainer(createLibrary());
     navbar.classList.remove("homeNavbar");
 })
@@ -66,24 +59,7 @@ libraryBtn.addEventListener("click", () => {
 //     changeContent(newBookForm);
 // })
 
-bookBtns.forEach(btn => {
-    btn.addEventListener("click", (event) => {
-        // on click change icon and color
-        if(btn.classList.contains("done")){
-            console.log("yellow");
-            btn.classList.remove("done");
-            btn.removeChild(btn.firstChild);
-            btn.innerHTML = "<span class='material-symbols-outlined'>auto_stories</span>";
-            btn.setAttribute("style", "background-color: #FFCA3C;");
-        }else {
-            console.log("green");
-            btn.classList.add("done")
-            btn.removeChild(btn.firstChild);
-            btn.innerHTML = "<span class='material-symbols-outlined'>done</span>";
-            btn.setAttribute("style", "background-color: #9CE5B0;");
-        }
-    })
-});
+
 
 
 
@@ -108,6 +84,13 @@ function createContentContainer(contentToLoad){
     newContent.setAttribute("id", "contentContainer");
     newContent.innerHTML = appendContent(contentToLoad);
     scrollBox.append(newContent);
+
+    if(contentToLoad === createLibrary()){
+        console.log("loading library");
+        createBookCardBtnEventListeners();
+    }else {
+        console.log("loading other screen");
+    }
 };
 // append custom content to div
 function appendContent(screenToLoad) {
@@ -133,4 +116,29 @@ function createLibrary(){
     }
 
     return '<ul class="bookList">' + liElements + '</ul>';
+}
+
+
+function createBookCardBtnEventListeners(){
+    // book card btns
+    const bookBtns = document.querySelectorAll(".read-btn");
+
+    bookBtns.forEach(btn => {
+        btn.addEventListener("click", (event) => {
+            // on click change icon and color
+            if(btn.classList.contains("done")){
+                console.log("yellow");
+                btn.classList.remove("done");
+                btn.removeChild(btn.firstChild);
+                btn.innerHTML = "<span class='material-symbols-outlined'>auto_stories</span>";
+                btn.setAttribute("style", "background-color: #FFCA3C;");
+            }else {
+                console.log("green");
+                btn.classList.add("done")
+                btn.removeChild(btn.firstChild);
+                btn.innerHTML = "<span class='material-symbols-outlined'>done</span>";
+                btn.setAttribute("style", "background-color: #9CE5B0;");
+            }
+        })
+    });
 }
